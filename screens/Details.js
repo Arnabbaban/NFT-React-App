8e9a6a58d725ad98d714dd0c6335019ddd4473a4
Arnabbaban <arnabghosh2406@gmail.com>
2022-12-13 10:29:16 +0000

    Done

--- screens/Details.js
+++ screens/Details.js
@@ -1,6 +1,14 @@
-import { Text } from "react-native";
-
-const Details = () => {
+import { Text,  View , SafeAreaView, Image, StatusBar, FlatList } from "react-native";
+import { COLORS, SIZES , SHADOWS, FONT, assets } from "../constants";
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from "../components";
+const Details = ( route, navigation) => {
+  const { data } = route.params;
+  console.log
+  (`-------------------------------------------------------------------------------`)
+  console.log(data);
+  console.log
+  (`-------------------------------------------------------------------------------`)
+  
   return (
     <Text>Details</Text>
   )
